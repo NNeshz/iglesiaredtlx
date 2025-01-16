3c35a060b6e57851fcbd7ab068093ce93529962d
Nefta Hernandez <neftaligow01@gmail.com>
2025-01-16 01:32:19 -0600

    chore: 🔄 Update Astro configuration to use Vercel adapter
    - Replaced the Node adapter with the Vercel serverless adapter in `astro.config.mjs`.
    - This change optimizes the deployment process for serverless environments.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [tailwind()],
 });
