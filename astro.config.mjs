dae5021310055218d56572cb9520231acb96128f
Nefta Hernandez <neftaligow01@gmail.com>
2025-01-16 10:33:55 -0600

    fix: 🐛 Delete @astro/node to aoid SSR problem
    Delete node from our project and change the astro.config file to make it
    work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
-  output: 'server', // Correcto para usar lógica del lado del servidor.
-  adapter: node({
-    mode: 'standalone', // Standalone es ideal para manejar lógica personalizada.
-  }),
   integrations: [tailwind()],
 });
